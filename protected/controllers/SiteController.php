3aa7b95bdb3c070aaded8dfb50202b9ef90c29d5
bercium <bercium@gmail.com>
2018-11-25 22:33:56 +0100

    Minor fixes

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -431,8 +431,7 @@ EOD;
                               ->queryAll();
         $next_shows = array();
         foreach ($next as $show){
-//          if (isset($next_shows[$show['channel_id']])) 
-            $next_shows[$show['channel_id']] = $show;
+          if (!isset($next_shows[$show['channel_id']])) $next_shows[$show['channel_id']] = $show;
         }
 
         $categories = Category::model()->findAll('id<>14 AND id<>15 AND id<>5 ORDER BY name');
